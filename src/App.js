8b8737841eec50a906db9b21a45da7e391c5e8f5
Pham Dinh Thoai <15130180@st.hcmuaf.edu.vn>
2018-08-08 09:55:30 +0700

    Giao diện ban đầu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,123 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>
-                    learn reactjs + redux
-                </h1>
-            </div>
+            <Fragment>
+                <div className="container">
+                    <div className="row mt-4">
+                        <div className="col-12">
+                            <h1 className="text-center">
+                                Quản lý công việc
+                            </h1>
+                        </div>
+                    </div>
+                    <div className="row mt-5">
+                        <div className="col-12 col-lg-4 mb-4">
+                            <div className="card">
+                                <div className="card-header">
+                                    Thêm công việc
+                                </div>
+                                <div className="card-body">
+                                    <form>
+                                        <div className="form-group">
+                                            <input type="text" className="form-control" />
+                                        </div>
+                                        <div className="form-group">
+                                            <select className="form-control">
+                                                <option>Chưa làm</option>
+                                                <option>Đang làm</option>
+                                                <option>Đã làm</option>
+                                            </select>
+                                        </div>
+                                        <div className="form-group">
+                                            <input type="submit" value="Lưu lại" className="btn btn-success mr-2" />
+                                            <input type="button" value="Hủy" className="btn btn-warning" />
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-12 col-lg">
+                            <div className="row">
+                                <div className="col-auto mb-4">
+                                    <button className="btn btn-primary">Thêm công việc</button>
+                                </div>
+                                <div className="col-auto mb-4">
+                                    <form>
+                                        <div className="input-group">
+                                            <input type="text" className="form-control border border-primary" />
+                                            <div className="input-group-append">
+                                                <button className="btn btn-primary" type="button">Tìm kiếm</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div className="col-auto mb-4">
+                                    <div className="dropdown">
+                                        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Sắp xếp
+                                        </button>
+                                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                            <a className="dropdown-item">Tên A-Z</a>
+                                            <a className="dropdown-item">Tên Z-A</a>
+                                            <hr />
+                                            <a className="dropdown-item">Trạng thái A-Z</a>
+                                            <a className="dropdown-item">Trạng thái Z-A</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row mb-2">
+                                <div className="col-12 text-center">
+                                    <h4>
+                                        Danh sách công việc
+                                    </h4>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-12">
+                                    <table className="table table-bordered text-center">
+                                        <thead>
+                                            <tr>
+                                                <th width="10%" scope="col">STT</th>
+                                                <th scope="col">Công việc</th>
+                                                <th width="25%" scope="col">Trạng thái</th>
+                                                <th width="20%" scope="col">Chức năng</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row"></th>
+                                                <td>
+                                                    <form>
+                                                        <input type="text" className="form-control" />
+                                                    </form>
+                                                </td>
+                                                <td>
+                                                    <select className="form-control">
+                                                        <option>Tất cả</option>
+                                                        <option>Chưa làm</option>
+                                                        <option>Đang làm</option>
+                                                        <option>Đã làm</option>
+                                                    </select>
+                                                </td>
+                                                <td></td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">1</th>
+                                                <td>Jacob</td>
+                                                <td>Thornton</td>
+                                                <td>@fat</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Fragment>
         );
     }
 }
